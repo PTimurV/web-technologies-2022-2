c65353a277fc1510ce4ceed0f4f148e29f5db363
Тимур <stud0000222598@study.utmn.ru>
2022-05-25 15:29:14 +0500

    9 задание

--- index.php
+++ index.php
@@ -1,184 +1,60 @@
-<!DOCTYPE html>
-<html lang='ru'>
-<?php
-
-function first($a, $b) {
-    if($a >= 0 && $b >= 0) {
-        return $a - $b;
-    }
-    else if($a < 0 && $b < 0) {
-        return $a * $b;
-    }
-    else {
-        return $a + $b;
-    }
-
-}
-
-function second() {
-    $a = mt_rand(0, 15);
-    $str="";
-    for ($i = $a; $i <= 15; $i++) {
-        switch ($i) {
-            default:
-                $str .= $i . ' ';
-        }
-    }
-    echo $str;
-}
-
-function third($arg1, $arg2, $operation){
-    switch ($operation){
-        case 'Сложение': return $arg1 + $arg2;
-        case 'Разность': return $arg1 - $arg2;
-        case 'Произведение': return $arg1 * $arg2;
-        case 'Деление': return $arg1 / $arg2;
-        default: return 'Ошибка';
-    }
-}
-
-$year = date('Y');
-
-function fifth() {
-    $i = 0;
-
-    do {
-        if ($i == 0) {
-            echo $i . ' – это ноль' . '<br>';
-        } elseif ($i% 2 == 1) {
-            echo $i . ' – нечетное число' . '<br>';
-        } else {
-            echo $i . ' – четное число' . '<br>';;
-        }
-        $i++;
-    } while ($i <= 10);
-
-}
-
-function sixth() {
-    $obls = [
-        'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
-        'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
-        'Рязанская область' => ['Рязань', 'Касимов', 'Рыбное', 'Ряжск']
-    ];
-
-    foreach ($obls as $obl => $city) {
-        echo $obl.':'.'<br>';
-        for ($i = 0; $i < count($city); $i++){
-            if($i == count($city) - 1) {
-                echo $city[$i] .'.';
-            } else {
-                echo $city[$i] .', ';
-            }
-        }
-        echo '<br>';
-    }
-}
-
-function seventh($str) {
-    $a = [
-        'a' => 'a',
-        'б' => 'b',
-        'в' => 'v',
-        'г' => 'g',
-        'д' => 'd',
-        'е' => 'e',
-        'ё' => 'yo',
-        'ж' => 'zh',
-        'з' => 'z',
-        'и' => 'i',
-        'й' => 'y',
-        'к' => 'k',
-        'л' => 'l',
-        'м' => 'm',
-        'н' => 'n',
-        'о' => 'o',
-        'п' => 'p',
-        'р' => 'r',
-        'с' => 's',
-        'т' => 't',
-        'у' => 'u',
-        'ф' => 'f',
-        'х' => 'h',
-        'ц' => 'c',
-        'ч' => 'ch',
-        'ш' => 'sh',
-        'щ' => 'sch',
-        'ъ' => '',
-        'ы' => 'y',
-        'ь' => '',
-        'э' => 'e',
-        'ю' => 'yu',
-        'я' => 'ya',
-    ];
-
-    return strtr($str, $a);
-}
-
-function eight() {
-    $arr = [
-        'mersedes' => ['E-class' => ['Трехдверные' =>['a', 's'], 'Пятидверные'],
-            'AMG'],
-        'bmw' => ['M-5' => ['Sport', 'F'], 'X-6'
-        => ['Sport']]
-    ];
-    return '<ul>' . menu($arr) . '</ul>';
-}
-
-function menu($arr){
-    $struct = '';
-
-    foreach($arr as $item => $value) {
-        if(!is_array($value)) {
-            $struct .= '<li>' . $value . '</li>';
-        } else {
-            $struct .= '<li>' . $item . '</li>';
-            $struct .= '<ul>' . menu($value) . '</ul>';
-        }
-
-    }
-
-    return $struct;
-}
-
-
-?>
+<!doctype html>
+<html lang="ru">
 <head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>hi</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test title</title>
 </head>
 <body>
+<?php
+date_default_timezone_set('Asia/Yekaterinburg');
 
-<h1> Задание 1: </h1>
-<h2>числа 3 и -5: <?= first(3, -5) ?></h2>
-<h2>числа 6 и 4: <?= first(6, 4) ?></h2>
-<h2>числа -2 -7: <?= first(-2, -7) ?></h2>
+function photo() {
+    $dir = './images';
+    $fil = scandir($dir);
 
-<h1> Задание 2: </h1>
-<h2> <?= second() ?></h2>
+    for ($i = 2; $i < count($fil); $i++) {
+        $path= $dir . DIRECTORY_SEPARATOR . $fil[$i];
+        echo '<a href=' . $path .'><img src=' . $path . ' width=300px height=300px style="margin: 10px;"></a>';
+    }
+}
+photo();
 
-<h1> Задание 3: </h1>
-<h2>Аргументы 3 и 5</h2>
-<h2> Сложение <?= third(3, 5, 'Сложение') ?></h2>
-<h2> Разность <?= third(3, 5, 'Разность') ?></h2>
-<h2> Произведение <?= third(3, 5, 'Произведение') ?></h2>
-<h2> Деление <?= third(3, 5, 'Деление') ?></h2>
+function logger() {
+    $log = __DIR__ . DIRECTORY_SEPARATOR . 'logs';
+    $log_files = scandir($log);
 
-<h1> Задание 4: </h1>
-<h2> Текущий год: <?= $year ?></h2>
+    if(!file_exists($log)) {
 
-<h1> Задание 5: </h1>
-<h2> <?= fifth() ?></h2>
+        mkdir($log, 0700, true);
+    }
 
-<h1> Задание 6: </h1>
-<h2> <?= sixth() ?></h2>
+    $log_count = count($log_files) - 2;
 
-<h1> Задание 7: </h1>
-<h2> <?= seventh('привет мир') ?></h2>
+    $date = date('c');
 
-<h1> Задание 8: </h1>
-<div> <?= eight() ?></div>
+    if($log_count === 0) {
+        file_put_contents($log . DIRECTORY_SEPARATOR . 'log.txt', $date . PHP_EOL, FILE_APPEND);
+    } else {
+        $last_log = end($log_files);
+
+        $first_log = explode(PHP_EOL,file_get_contents($log .
+            DIRECTORY_SEPARATOR . 'log.txt'));
+
+        if(count($first_log) - 1 == 10) {
+            $log_number = +preg_replace('/[^0-9]/', '', $last_log) + 1;
+            file_put_contents($log . DIRECTORY_SEPARATOR . 'log' .
+                $log_number . '.txt', file_get_contents($log .
+                    DIRECTORY_SEPARATOR . 'log.txt') . PHP_EOL);
+            file_put_contents($log . DIRECTORY_SEPARATOR . 'log.txt', $date . PHP_EOL);
+        } else {
+            file_put_contents($log . DIRECTORY_SEPARATOR .  'log.txt', $date . PHP_EOL, FILE_APPEND);
+        }
+    }
+}
+logger();
+?>
 </body>
 </html>
